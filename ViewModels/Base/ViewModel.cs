50c163537a89bf236ad62dc36aa1c767b65ba9dd
Дмитрий Шошин <shoshin.142@yandex.ru>
2022-10-09 15:41:45 +0300

    ViewModel Base

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoPlayer.ViewModels.Base
+{
+    internal class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+            else
+            {
+                field = value;
+                OnPropertyChanged(PropertyName);
+                return true;
+            }
+        }
+    }
+}
